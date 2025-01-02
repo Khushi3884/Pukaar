eae7acf0f285900bde15e12cd9df4e6199675401
Khushi3884 <163239315+Khushi3884@users.noreply.github.com>
2025-01-02 11:54:08 +0530

    Create Navbar.js
    Navigation component

---
+++ frontend/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>PUKaR</h1>
+      <button>Home</button>
+      <button>Notifications</button>
+    </nav>
+  );
+};
+
+export default Navbar;
